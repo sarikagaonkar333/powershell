e77b08e8fee7d8a719977a017c9fa5541520bb77
sarikagaonkar333 <48145525+sarikagaonkar333@users.noreply.github.com>
2019-05-09 09:58:47 +0530

    Add files via upload

---
+++ top10Process.ps1
@@ -0,0 +1,10 @@
+﻿
+
+get-help Get-Process -ShowWindow
+
+#top10 process
+Get-Process | Sort-Object -Descending | select -First 10| select name,@{label="ws(mb)";e={$_.ws/1mb}}
+
+Get-Process | Where-Object {$_.WorkingSet -gt 20000000}
+
+Get-Process | where {$_.mainWindowTitle} | Format-Table id, name, mainwindowtitle -autosize
